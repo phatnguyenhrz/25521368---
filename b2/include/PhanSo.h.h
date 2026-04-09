39d9e7b9e965e048a5d1acc8273eb2c1cdc48b93
phatnguyenhrz <phatgaming0208@gmail.com>
2026-04-09 11:48:42 +0700

    Add files via upload

---
+++ b2/include/PhanSo.h.h
@@ -0,0 +1,16 @@
+#ifndef PHANSO_H_H
+#define PHANSO_H_H
+
+
+class PhanSo.h
+{
+    public:
+        PhanSo.h();
+        virtual ~PhanSo.h();
+
+    protected:
+
+    private:
+};
+
+#endif // PHANSO_H_H
---
+++ b2/src/PhanSo.h.cpp
@@ -0,0 +1,11 @@
+#include "PhanSo.h.h"
+
+PhanSo.h::PhanSo.h()
+{
+    //ctor
+}
+
+PhanSo.h::~PhanSo.h()
+{
+    //dtor
+}
